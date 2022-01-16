a8e1c9db9ca70901e8bfcf87ae331920000d9fa3
JosephSB <jguku2002@gmail.com>
2022-01-16 13:11:52 -0500

    docker integrado

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 
 const app = express();
 
-app.set("port", process.env.PORT || 5000);
+app.set("port", process.env.PORT || 3000);
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
